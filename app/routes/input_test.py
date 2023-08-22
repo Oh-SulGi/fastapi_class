7788100b85306b84ddda812b963e0376f5ecaa3a
Oh-SulGi <dhtmfrl0615@naver.com>
2023-08-22 14:12:13 +0900

    0.0.1

--- app/routes/input_test.py
+++ app/routes/input_test.py
@@ -4,6 +4,7 @@ from fastapi import Request
 from fastapi.responses import HTMLResponse
 
 import os
+import pandas as pd
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -29,6 +30,7 @@ async def upload_file(request: Request, file: UploadFile = File(...)):
     
     # 파일 저장
     file_path = os.path.join(upload_dir, file.filename)
+    print(file_path)
     with open(file_path, "wb") as buffer:
         while True:
             data = file.file.read(1024)
@@ -44,4 +46,37 @@ async def upload_file(request: Request, file: UploadFile = File(...)):
                                        "result": result,
                                        "save_path": save_path})
 
-         
+@router.post('/upload_files_docs')
+async def upload_docs(request: Request, user_id: str, file: UploadFile = File(...)):
+    # 파일을 저장할 디렉토리 체크 후 생성
+    upload_dir = 'media/picture/'
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+    
+    # 파일 저장
+    file_path = os.path.join(upload_dir, user_id + '_' + file.filename)
+    df_path = 'media/DB/image_db.csv'
+
+    image_db = {
+        'id' : [user_id],
+        'file_path' : [file_path],
+        'file_name' : [file.filename]
+    }
+    df = pd.DataFrame(image_db)
+    df.to_csv(df_path)
+
+    print(file_path)
+    with open(file_path, "wb") as buffer:
+        while True:
+            data = file.file.read(1024)
+            if not data:
+                break
+            buffer.write(data)
+
+    result = "Upload success: {}".format(file.filename)
+    print(file.filename)
+    save_path = upload_dir + file.filename
+    return templates.TemplateResponse("input_test.html", 
+                                      {"request": request, 
+                                       "result": result,
+                                       "save_path": save_path})
